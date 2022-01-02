357c54e7031a5a4a13c12bfc208865ca12783e6a
chrop01 <91630967+chrop01@users.noreply.github.com>
2022-01-02 13:43:17 +0100

    Added more blocks

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,7 +10,21 @@ public class Main {
         int hashvalue = statement.hashCode();
 
         ArrayList<Block> blockchain = new ArrayList<Block>();
+
+
         String[] initialvalues = {"$700, $550"};
         Block firstblock = new Block(initialvalues, 0);
+        blockchain.add(firstblock);
+
+        System.out.println("First block: " + firstblock.toString());
+        System.out.println("Chain: " + blockchain .toString());
+
+        String[] Send = {"$50, $10"};
+        Block secondblock = new Block(Send, firstblock.getBlockHash());
+        blockchain.add(secondblock);
+
+
+        System.out.println("Second block: " + secondblock.toString());
+        System.out.println("Chain: " + blockchain .toString());
     }
 }
