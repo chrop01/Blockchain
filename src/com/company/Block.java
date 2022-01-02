7e2d16fb76858b3b2a7ed03f325f6e9522607ad0
chrop01 <91630967+chrop01@users.noreply.github.com>
2022-01-02 21:02:35 +0100

    Functional blockchain

--- src/com/company/Block.java
+++ src/com/company/Block.java
@@ -1,53 +1,62 @@
 package com.company;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import org.apache.commons.lang3.StringUtils;
+import java.util.Date;
+
 
 public class Block {
 
-    private String[] transcactions; //external
-    private int blockHash;
-    private int previousBlockHash; //external
+    public String hash;
+    public String previousHash;
+    public String merkleRoot;
+    public ArrayList<Transaction> transactions = new ArrayList<Transaction>(); //our data will be a simple message.
+    public long timeStamp; //as number of milliseconds since 1/1/1970.
+    public int nonce;
 
+    //Block Constructor.
+    public Block(String previousHash ) {
+        this.previousHash = previousHash;
+        this.timeStamp = new Date().getTime();
 
-    public Block(String[] transcactions, int previousBlockHash) {
-        super();
-        this.transcactions = transcactions;
-        this.previousBlockHash = previousBlockHash;
-        //1 -> hash from the array of transactions | 2-> previous hash
-        this.blockHash = Arrays.hashCode(new int[] {Arrays.hashCode(transcactions),this.previousBlockHash});
-
+        this.hash = calculateHash(); //Making sure we do this after we set the other values.
     }
 
-    public String[] getTranscactions() {
-        return transcactions;
+    //Calculate new hash based on blocks contents
+    public String calculateHash() {
+        String calculatedhash = StringUtil.applySha256(
+                previousHash +
+                        Long.toString(timeStamp) +
+                        Integer.toString(nonce) +
+                        merkleRoot
+        );
+        return calculatedhash;
     }
 
-    public void setTranscactions(String[] transcactions) {
-        this.transcactions = transcactions;
+    //Increases nonce value until hash target is reached.
+    public void mineBlock(int difficulty) {
+        merkleRoot = StringUtil.getMerkleRoot(transactions);
+        String target = StringUtil.getDificultyString(difficulty); //Create a string with difficulty * "0"
+        while(!hash.substring( 0, difficulty).equals(target)) {
+            nonce ++;
+            hash = calculateHash();
+        }
+        System.out.println("Block Mined!!! : " + hash);
     }
 
-    public int getBlockHash() {
-        return blockHash;
+    //Add transactions to this block
+    public boolean addTransaction(Transaction transaction) {
+        //process transaction and check if valid, unless block is genesis block then ignore.
+        if(transaction == null) return false;
+        if((previousHash != "0")) {
+            if((transaction.processTransaction() != true)) {
+                System.out.println("Transaction failed to process. Discarded.");
+                return false;
+            }
+        }
+        transactions.add(transaction);
+        System.out.println("Transaction Successfully added to Block");
+        return true;
     }
 
-    public void setBlockHash(int blockHash) {
-        this.blockHash = blockHash;
-    }
-
-    public int getPreviousBlockHash() {
-        return previousBlockHash;
-    }
-
-    public void setPreviousBlockHash(int previousBlockHash) {
-        this.previousBlockHash = previousBlockHash;
-    }
-
-    @Override
-    public String toString() {
-        return "Block{" +
-                "transcactions=" + Arrays.toString(transcactions) +
-                ", blockHash=" + blockHash +
-                ", previousBlockHash=" + previousBlockHash +
-                '}';
-    }
 }
